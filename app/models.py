6243d50e4a7f331e0a4b1f23f8b08caf0817b229
= <hjxwho@gmail.com>
2014-12-07 02:34:40 -0500

    Add model `Post`

--- app/models.py
+++ app/models.py
@@ -7,3 +7,22 @@ class User(db.Model):
     password = db.Column(db.String(64))
     phoneNumber = db.Column(db.String(64))
     email = db.Column(db.String(64))
+    posts = db.relationship('Post', backref='user')
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    content = db.Column(db.Text)
+    done = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'postID' : self.id,
+            'title' : self.title,
+            'content' : self.content,
+            'done' : self.done,
+            'userID' : self.user_id
+        }
